81b9608c1e92ac71114476982fb7dc6d9e01ddba
Arronax <alexandre.petit.qin@gmail.com>
2021-10-18 16:56:36 +0200

    first commit

---
+++ src/test/java/coding/kata/BankAccountShould.java
@@ -0,0 +1,20 @@
+package coding.kata;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class BankAccountShould {
+
+    @Test
+    public void return_white_wins_when_white_has_a_better_high_card_than_black() {
+
+    }
+}
